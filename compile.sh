739a1fab11d8e6a84b6c6339e6369ef69737bcf2
Koshu <mail@koshu.de>
2012-09-23 17:15:29 +0200

    Fixed buildsystem

--- compile.sh
+++ compile.sh
@@ -1,3 +1,3 @@
 export CCOMPILER=${HOME}/Android/clockworkmod/prebuilt/linux-x86/toolchain/arm-eabi-4.4.3/bin/arm-eabi-
-make ARCH=arm CROSS_COMPILE=$CCOMPILER menuconfig
+make ARCH=arm CROSS_COMPILE=$CCOMPILER oldconfig
 #make ARCH=arm CROSS_COMPILE=$CCOMPILER -j`grep 'processor' /proc/cpuinfo | wc -l`
--- include/generated/utsrelease.h
+++
@@ -1,1 +0,0 @@
-#define UTS_RELEASE "2.6.39.4-g9b8008b"
